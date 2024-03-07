4b8ac673827467be21b01b520a6585429b74403f
Grampachampa <grampachampa@gmail.com>
2024-03-07 11:08:28 +0100

    added sh command

---
+++ run_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --time=24:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=defq
+#SBATCH -C Titan
+#SBATCH --gres=gpu:1
+
+module load cuda12.1/toolkit
+module load cuDNN/cuda12.1
+
+
+source $HOME/.bashrc
+conda activate ML
+
+cd /var/scratch/tbt204/TransferLearning
+
+
+## python selfplay_rework.py --parameter
+
+python <<EOF
+import torch
+print(torch.cuda.is_available())
+EOF
