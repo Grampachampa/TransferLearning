6e21fbec1503683b606a9eafce17216905411b7a
Grampachampa <grampachampa@gmail.com>
2024-03-09 13:22:06 +0100

    4 stacks

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def train(path = None, epsilon = None):
     env = GrayScaleObservation(env)
     env = ResizeObservation(env, shape=(84, 128))
 
-    num_stacks = 3
+    num_stacks = 4
 
     env = gym.wrappers.FrameStack(env, num_stack=num_stacks)
 
