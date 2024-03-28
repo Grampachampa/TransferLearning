d2c25094ac0c0b87a6a3a22d81770f891c61f087
Grampachampa <grampachampa@gmail.com>
2024-03-28 14:07:25 +0100

    final commit with data

---
+++ shell commands/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=compile_data
+#SBATCH --time=24:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=defq
+#SBATCH -C Titan
+#SBATCH --gres=gpu:1
+
+module load cuda12.1/toolkit
+
+source $HOME/.bashrc
+conda activate ML
+
+cd /var/scratch/tbt204/TransferLearning
+
+
+python ../compile_data.py
+
