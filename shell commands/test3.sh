93d8d8d27c28e9d4e774e2db5df33c3bd44385a8
Grampachampa <grampachampa@gmail.com>
2024-03-29 11:33:02 +0100

    amended tests

--- shell commands/test3.sh
+++ shell commands/test3.sh
@@ -3,7 +3,9 @@
 #SBATCH --time=00:20:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
-#SBATCH --partition=longq
+#SBATCH --partition=defq
+#SBATCH -C Titan
+#SBATCH --gres=gpu:1
 
 module load cuda12.1/toolkit
 
